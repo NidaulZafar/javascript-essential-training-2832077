766712b25ceed2a57af0b55ad707608a8a80d344
N Zafar <n_zafar_007@hotmail.com>
2022-05-28 14:53:10 +0200

    2 practice exercises done

--- Practice/03_09/script.js
+++ Practice/03_09/script.js
@@ -12,6 +12,7 @@ const backpack = {
   volume: 30,
   color: "grey",
   pocketNum: 15,
+  lidOpen: false,
   strapLength: {
     left: 26,
     right: 26,
@@ -23,4 +24,37 @@ const backpack = {
     this.strapLength.left = lengthLeft;
     this.strapLength.right = lengthRight;
   },
+  volSize: function (size) {
+    this.volume = size;
+  },
+  rename: function (newName) {
+    this.name = newName;
+  },
+  rePaint: function (newColor) {
+    this.color = newColor;
+  },
+  addedPockets: function (pockets) {
+    this.pocketNum += pockets;
+  },
 };
+console.log(backpack);
+console.log(backpack.toggleLid(false));
+console.log(backpack.lidOpen);
+console.log(backpack.toggleLid(true));
+console.log(backpack.lidOpen);
+console.log(backpack.toggleLid(3));
+console.log(backpack.lidOpen);
+console.log(`original volume ${backpack.volume}`);
+console.log(backpack.volSize(50));
+console.log(`modified volume ${backpack.volume}`);
+console.log(backpack.volSize(100));
+console.log(`final volume ${backpack.volume}`);
+console.log(`originally called ${backpack.name}`);
+backpack.rename("Special");
+console.log(`now renamed as ${backpack.name}`);
+console.log(`original color ${backpack.color}`);
+backpack.rePaint("Purple");
+console.log(`now recolored as ${backpack.color}`);
+console.log(`originally had ${backpack.pocketNum}`);
+backpack.addedPockets(5);
+console.log(`5 pockets added and now has ${backpack.pocketNum}`);
